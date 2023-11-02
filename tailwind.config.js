bc1cc42d53c9bde8b4258f648d15518617a3e4bd
Arslan Ali <arslan.neni23@gmail.com>
2023-11-02 16:14:45 +0500

    login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "15px",
-      },
-
-      colors: {
-        accent: "#FF8F9C",
-        blackish: "#1b1b1b",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: [
+	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+	  extend: {
+		container: {
+		  center: true,
+		  padding: "15px",
+		},
+		colors: {
+		  accent: "#FF8F9C",
+		  blackish: "#1b1b1b",
+		},
+	  },
+	},
+	plugins: [
+	  require('@tailwindcss/forms'), // Add the @tailwindcss/forms plugin
+	],
+  };
+  
