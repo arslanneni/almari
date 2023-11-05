5089e1992375546e9cee636ee168137e18e35207
Arslan Ali <arslan.neni23@gmail.com>
2023-11-06 00:31:57 +0500

    Login and Registration

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,66 +1,29 @@
 import React from 'react';
-import Link from 'next/link';
-import styles from '../../public/styles/Login.module.css';
+import '../../public/styles/Login.css'
 
 export default function Login() {
+  // Handle the login button click
+  const handleLogin = () => {
+    // Replace this with your actual login logic
+    console.log('Login button clicked');
+  };
+
   return (
-    <div>
-      <section className={styles.container}>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className={styles.logo}>
-            Syringe
-          </a>
-          <div className={styles.card}>
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className={styles.title}>Sign in to your account</h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                <div className={styles.form-group}>
-                  <label htmlFor="email" className={styles.label}>
-                    Username
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className={styles.input}
-                    placeholder="Username"
-                    required=""
-                  />
-                </div>
-                <div className={styles.form-group}>
-                  <label htmlFor="password" className={styles.label}>
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className={styles.input}
-                    required=""
-                  />
-                </div>
-                <div className={styles.checkbox-group}>
-  <div className={styles.checkbox}></div>
-  <div className={styles.checkbox-label}>Remember me</div>
-</div>
-                <Link href="/forgot-password">
-                  <a className={styles.forgot-password}>Forgot password?</a>
-                </Link>
-                <button type="submit" className={styles.button}>
-                  Sign in
-                </button>
-                <p className={styles.sign-up-link}>
-                  Don’t have an account yet?{' '}
-                  <Link href="/clinicSignup">
-                    <a className="text-sky-600 font-semibold">Sign Up</a>
-                  </Link>
-                </p>
-              </form>
-            </div>
-          </div>
+    <div className="container">
+      <h2>Login</h2>
+      <form>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Enter your username" required />
         </div>
-      </section>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required />
+        </div>
+        <button className="custom-button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
--- pages/Signup/index.js
+++ pages/Signup/index.js
@@ -1,147 +1,33 @@
 import React from 'react';
+import '../../public/styles/Login.css';
 
 export default function Signup() {
-  const styles = {
-    container: {
-      display: 'flex',
-      minHeight: '80vh',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      padding: '2rem',
-    },
-    logo: {
-      display: 'block',
-      margin: '0 auto',
-      height: '2.5rem',
-      width: 'auto',
-    },
-    heading: {
-      marginTop: '2rem',
-      textAlign: 'center',
-      fontSize: '1.25rem',
-      fontWeight: 'bold',
-      color: '#333',
-    },
-    form: {
-      marginTop: '2rem',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '1rem',
-    },
-    label: {
-      fontSize: '0.875rem',
-      fontWeight: 'bold',
-      color: '#333',
-    },
-    input: {
-      width: '100%',
-      borderRadius: '0.25rem',
-      padding: '0.375rem',
-      border: '1px solid #ccc',
-      color: '#333',
-      boxShadow: 'none',
-      placeholder: {
-        color: '#666',
-      },
-      focus: {
-        ringWidth: '2px',
-        ringOffset: '2px',
-        ringColor: 'indigo',
-      },
-    },
-    button: {
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      borderRadius: '0.25rem',
-      padding: '0.375rem 1.5rem',
-      fontSize: '0.875rem',
-      fontWeight: 'bold',
-      color: 'white',
-      backgroundColor: 'indigo',
-      boxShadow: 'none',
-      hover: {
-        backgroundColor: 'indigo',
-      },
-    },
-    forgotPassword: {
-      fontSize: '0.875rem',
-      fontWeight: 'bold',
-      color: 'indigo',
-      hover: {
-        color: 'darkblue',
-      },
-    },
-    signUpLink: {
-      marginTop: '1rem',
-      fontSize: '0.875rem',
-      fontWeight: 'bold',
-      color: 'indigo',
-      hover: {
-        color: 'darkblue',
-      },
-    },
+  // Handle the signup button click
+  const handleSignup = () => {
+    // Replace this with your actual signup logic
+    console.log('Signup button clicked');
   };
 
   return (
-    <>
-      <div style={styles.container}>
-        <img
-          style={styles.logo}
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 style={styles.heading}>Sign in to your account</h2>
-        <form style={styles.form} action="#" method="POST">
-          <div>
-            <label style={styles.label} htmlFor="email">
-              Email address
-            </label>
-            <div>
-              <input
-                style={styles.input}
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                placeholder="Enter your email"
-              />
-            </div>
-          </div>
-          <div>
-            <div style={{ ...styles.label, display: 'flex', justifyContent: 'space-between' }}>
-              Password
-              <a style={styles.forgotPassword} href="#">
-                Forgot password?
-              </a>
-            </div>
-            <div>
-              <input
-                style={styles.input}
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                placeholder="Enter your password"
-              />
-            </div>
-          </div>
-          <div>
-            <button type="submit" style={styles.button}>
-              Sign in
-            </button>
-          </div>
-        </form>
-        <p style={styles.signUpLink}>
-          Not a member?{' '}
-          <a href="#" style={styles.signUpLink}>
-            Start a 14-day free trial
-          </a>
-        </p>
-      </div>
-    </>
+    <div className="container">
+      <h2>Sign Up</h2>
+      <form>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Enter your username" required />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required />
+        </div>
+        <button className="custom-button" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </form>
+    </div>
   );
 }
