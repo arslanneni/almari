46a1bd9239e2dc493af0fbfd380a369507fdc130
Arslan Ali <arslan.neni23@gmail.com>
2023-11-13 10:23:29 +0500

    Sign Up and Login

--- pages/Signup/index.js
+++ pages/Signup/index.js
@@ -1,100 +1,128 @@
-import React, { useEffect } from 'react';
-import Image from 'next/image';
+import React, { useEffect } from "react";
+import Image from "next/image";
 
 const Signup = () => {
-	useEffect(() => {
-	
-		import('bootstrap/dist/css/bootstrap.min.css');
+  useEffect(() => {
+    import("bootstrap/dist/css/bootstrap.min.css");
 
-		import('bootstrap/dist/js/bootstrap.bundle.min.js').then(() => {
-			
-		
-		});
-	}, []);
-	
-	const handleLogin = () => 
-	{
-		window.location.href='http://localhost:3000/Login'
-	}
-	return (
-		<>
-	 <section className="vh-100" style={{backgroundColor:'#eee'}}>
-	<div className="container h-100">
-		<div className="row d-flex justify-content-center align-items-center h-100">
-			<div className="col-lg-12 col-xl-11">
-				<div className="card text-black" style={{borderRadius:"25px"}}>
-					<div className="card-body p-md-5">
-						<div className="row justify-content-center">
-							<div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    import("bootstrap/dist/js/bootstrap.bundle.min.js").then(() => {});
+  }, []);
 
-								<p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+  const handleLogin = () => {
+    window.location.href = "http://localhost:3000/Login";
+  };
+  return (
+    <>
+      <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black" style={{ borderRadius: "25px" }}>
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Sign up
+                      </p>
 
-								<form className="mx-1 mx-md-4">
+                      <form className="mx-1 mx-md-4">
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="text"
+                              id="form3Example1c"
+                              className="form-control"
+                            />
+                            <label className="form-label" for="form3Example1c">
+                              Your Name
+                            </label>
+                          </div>
+                        </div>
 
-									<div className="d-flex flex-row align-items-center mb-4">
-										<i className="fas fa-user fa-lg me-3 fa-fw"></i>
-										<div className="form-outline flex-fill mb-0">
-											<input type="text" id="form3Example1c" className="form-control" />
-											<label className="form-label" for="form3Example1c">Your Name</label>
-										</div>
-									</div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              id="form3Example3c"
+                              className="form-control"
+                            />
+                            <label className="form-label" for="form3Example3c">
+                              Your Email
+                            </label>
+                          </div>
+                        </div>
 
-									<div className="d-flex flex-row align-items-center mb-4">
-										<i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-										<div className="form-outline flex-fill mb-0">
-											<input type="email" id="form3Example3c" className="form-control" />
-											<label className="form-label" for="form3Example3c">Your Email</label>
-										</div>
-									</div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              id="form3Example4c"
+                              className="form-control"
+                            />
+                            <label className="form-label" for="form3Example4c">
+                              Password
+                            </label>
+                          </div>
+                        </div>
 
-									<div className="d-flex flex-row align-items-center mb-4">
-										<i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-										<div className="form-outline flex-fill mb-0">
-											<input type="password" id="form3Example4c" className="form-control" />
-											<label className="form-label" for="form3Example4c">Password</label>
-										</div>
-									</div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              id="form3Example4cd"
+                              className="form-control"
+                            />
+                            <label className="form-label" for="form3Example4cd">
+                              Repeat your password
+                            </label>
+                          </div>
+                        </div>
 
-									<div className="d-flex flex-row align-items-center mb-4">
-										<i className="fas fa-key fa-lg me-3 fa-fw"></i>
-										<div className="form-outline flex-fill mb-0">
-											<input type="password" id="form3Example4cd" className="form-control" />
-											<label className="form-label" for="form3Example4cd">Repeat your password</label>
-										</div>
-									</div>
-
-									
-
-									<div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-										<button type="button" className="btn btn-primary btn-lg">Register</button>
-									</div>
-									<p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>
-		Already have an account?{' '}
-		<a href="#" onClick={handleLogin} style={{ color: '#393f81' }}>
-		  Login here
-		</a>
-	  </p>
-
-								</form>
-
-							</div>
-							<div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-								<Image src="/SignupImage.jpeg"
-								width={400}
-								height={400}
-									className="img-fluid" alt="Sample image"/> 
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-		</>
-	);
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Register
+                          </button>
+                        </div>
+                        <p
+                          className="mb-5 pb-lg-2"
+                          style={{ color: "#393f81" }}
+                        >
+                          Already have an account?{" "}
+                          <a
+                            href="#"
+                            onClick={handleLogin}
+                            style={{ color: "#393f81" }}
+                          >
+                            Login here
+                          </a>
+                        </p>
+                      </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <Image
+                        src="/SignupImage.jpeg"
+                        width={400}
+                        height={400}
+                        className="img-fluid"
+                        alt="Sample image"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Signup;
